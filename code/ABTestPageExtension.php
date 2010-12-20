cf37e11dcfc71a03aabe5e0e44c217db08847c55
mstephens <mstephens@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-12-20 04:58:41 +0000

    ENHANCEMENT: for alternate page data, switched from redirection to using update() to overright extended objects properties resulting in proper display on navigation
    git-svn-id: http://svn.silverstripe.com/modules/abtesting/trunk@115230 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/ABTestPageExtension.php
+++ code/ABTestPageExtension.php
@@ -14,14 +14,18 @@ class ABTestPageExtension extends DataObjectDecorator {
 		if ($experiment = ABTestExperiment::get_experiment_for_test_page($this->owner)) {
 			$experiment->determineVariation();
 			$experiment->hitTestedPage();
-			$experiment->redirectIfPageVariant();
+//			$experiment->redirectIfPageVariant();
 
 			$variation = $experiment->getVariation();
 
-// If we could use the alternate page as a customised object, then 
-//			if ($variation && $variation->Presentation == "AlternatePage") {
-//				$this->owner->setCustomisedObj($variation->AlternatePage());
-//			}
+			// @todo Separate the alternate page from the template selection in the model, so both
+			//  	 alternative content and template can be selected.
+
+			// If an alternate page is given, update the data of this page to the data of the
+			// alternate page, without writing back.
+			if ($variation && $variation->Presentation == "AlternatePage") {
+				$this->updatePageContentFromAlternate($variation);
+			}
 
 			if ($variation && $variation->Presentation == "AlternateTemplate") {
 				$templates = explode(",", $variation->AlternateTemplate);
@@ -35,12 +39,26 @@ class ABTestPageExtension extends DataObjectDecorator {
 			else if ($variation && $variation->Presentation == "DynamicTemplate" && class_exists("DynamicTemplate")) {
 				$this->owner->DynamicTemplateID = $variation->DynamicTemplateID;
 			}
-
-			// @todo 	If the page variant uses a dynamic template we force it here.
-			// @todo 	If the page variant uses an alternative template, we just force it into $this->templates['index']
 		}
 
 		// If this is a conversion page for any experiments, update stats accordingly.
 		ABTestExperiment::hit_conversion_pages($this->owner);
+	}
+
+	/**
+	 * When we're given an alternate page, we copy the attributes from the alternate page into $this->owner,
+	 * to simulate the page. Certain things we don't copy, such as ID, URLSegment, ClassName and anything with
+	 * a dot (because DataObject::update writes related objects, and we need to guarantee no state is changed.)
+	 * We do this because we can't replace the owner of this extension with another object, and redirection to
+	 * the alternate page typically causes the navigation to not work properly.
+	 */
+	public function updatePageContentFromAlternate($variation) {
+		if (!$variation->AlternatePage()) return;
+		$map = array();
+		foreach ($variation->AlternatePage()->toMap() as $key => $value) {
+			if (strpos($key, '.') === false && $key != "URLSegment" && $key != "ID")
+				$map[$key] = $value;
+		}
+		$this->owner->update($map);
 	}
 }
